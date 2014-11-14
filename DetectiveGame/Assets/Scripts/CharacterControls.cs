c72aa20e27cf29e528237410337be6f2a390e490
Theis Berthelsen <theisberthelsen@gmail.com>
2014-11-14 17:06:08 +0100

    Animation fixed and loose win screens updated

--- DetectiveGame/Assets/Scripts/CharacterControls.cs
+++ DetectiveGame/Assets/Scripts/CharacterControls.cs
@@ -12,12 +12,20 @@ public class CharacterControls : MonoBehaviour {
 
 	private bool grounded = false;
 	private Animation anim;
+	protected Animator animator;
+	public float DirectionDampTime = .25f;
 		
 	void Awake () {
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
 	}
+
 	
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+
+	}
 	void FixedUpdate () {
 
 		if (GameObject.FindWithTag ("Player").GetComponent<MouseLook>().cutSceneOn == true) {
@@ -43,11 +51,37 @@ public class CharacterControls : MonoBehaviour {
 			
 			grounded = false;
 
-			if (Input.GetAxis ("Horizontal") > 0.1f || Input.GetAxis ("Vertical") > 0.1f) {
-				GameObject.Find("Player").animation.CrossFade ("walking_1");
-			} else if (Input.GetAxis ("Horizontal") < 0.1f || Input.GetAxis ("Vertical") < 0.1f) {
-				GameObject.Find("Player").animation.Stop ("walking_1");
-			}
+		//	if (Input.GetAxis ("Horizontal") > 0.1f || Input.GetAxis ("Vertical") > 0.1f) {
+		//		GameObject.Find("Player").animation.CrossFade ("walking_1");
+
+		//	} else if (Input.GetAxis ("Horizontal") < 0.1f || Input.GetAxis ("Vertical") < 0.1f) {
+		//		GameObject.Find("Player").animation.CrossFade ("31_08");
+		//	}
+			if(animator)
+			{
+				//get the current state
+				AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+				
+				//if we're in "Run" mode, respond to input for jump, and set the Jump parameter accordingly. 
+				if(stateInfo.nameHash == Animator.StringToHash("Base Layer.RunBT"))
+				{
+					if(Input.GetButton("Fire1")) 
+						animator.SetBool("Jump", true );
+				}
+				else
+				{
+					animator.SetBool("Jump", false);				
+				}
+				
+				float h = Input.GetAxis("Horizontal");
+				float v = Input.GetAxis("Vertical");
+				
+				//set event parameters based on user input
+				animator.SetFloat("Speed", h*h+v*v);
+				animator.SetFloat("Direction", h, DirectionDampTime, Time.deltaTime);
+			}		
+
+
 		}
 	}
 	
